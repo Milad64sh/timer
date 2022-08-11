c886719bd60afb5427aaba62347946c5b59093d8
Milad64sh <m.shalikarian@gmail.com>
2022-08-10 20:08:54 -0700

    js

---
+++ app.js
@@ -0,0 +1,9 @@
+const start = document.getElementById('start');
+const stop = document.getElementById('stop');
+const reset = document.getElementById('reset');
+
+const wm = document.getElementById('w-minutes');
+const ws = document.getElementById('w-seconds ');
+
+const bm = document.getElementById('b-minutes');
+const bs = document.getElementById('b-seconds');
